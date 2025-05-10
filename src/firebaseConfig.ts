10b2adb45df0258f17acdfd71805992d12144051
pawandeore <98394964+pawandeore@users.noreply.github.com>
2025-05-10 16:22:56 +0530

    env added

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB44zqaGO0HNd2GrR6wg-nSmyJ9UPqZOqQ",
-    authDomain: "auth-23943.firebaseapp.com",
-    projectId: "auth-23943",
-    storageBucket: "auth-23943.firebasestorage.app",
-    messagingSenderId: "1031276381784",
-    appId: "1:1031276381784:web:4d3de8641a1af2266fac2d",
-    measurementId: "G-4JYWZ15L2Z"
+    apiKey:  import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_ANALYTICS_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
